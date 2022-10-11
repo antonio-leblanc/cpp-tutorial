6faacfd231182233876dff43ecad1beccde0e5be
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2022-10-11 20:09:22 -0300

    add another file

--- cmake_tutorial/main.cpp
+++ cmake_tutorial/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "FirePropagation.h"
+#include "FireModel.h"
 
 
 int main(){
   std::cout << "VAMO TIME" << std::endl;
+  LabresModel().getParams();
   FirePropagation().propagate();
   return 0;
 }
---
+++ cmake_tutorial/src/FireModel.cpp
@@ -0,0 +1,6 @@
+#include "FireModel.h"
+#include <iostream>
+
+void LabresModel::getParams(){
+    std::cout << "parametros bons!\n";
+  };
