40cd08a7686c3ae0ab00eda35ca4dee36e86367f
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2022-10-20 09:30:26 -0300

    lib with many cpp files

--- cmake_tutorial/main.cpp
+++ cmake_tutorial/main.cpp
@@ -2,12 +2,14 @@
 #include "FirePropagation.h"
 #include "FireModel.h"
 #include "DataBroker.h"
+#include "FluxModel.h"
 
 
 int main(){
   std::cout << "VAMO TIME" << std::endl;
   DataBroker().loadData();
   LabresModel().getParams();
+  FluxModel().fluxo();
   FirePropagation().propagate();
   return 0;
 }
---
+++ cmake_tutorial/src/Models/FluxModel.cpp
@@ -0,0 +1,6 @@
+#include "FluxModel.h"
+#include <iostream>
+
+void FluxModel::fluxo(){
+    std::cout << "Olha o fluxo\n";
+  };
