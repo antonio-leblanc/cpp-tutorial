4cb1e2f50a0cd1a4a3bf38fe06abdcccfaa6f7a3
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2025-04-23 10:06:28 -0300

    adder

---
+++ py-cpp/minimal-cpp/adder.cpp
@@ -0,0 +1,6 @@
+#include "adder.h" // Include the header to ensure consistency
+
+// Definition (implementation) of the add function
+int add(int a, int b) {
+    return a + b;
+}
---
+++ py-cpp/minimal-cpp/adder.h
@@ -0,0 +1,7 @@
+#ifndef ADDER_H // Include guards: prevent multiple inclusions
+#define ADDER_H
+
+// Declaration of the function we want to expose
+int add(int a, int b);
+
+#endif // ADDER_H
